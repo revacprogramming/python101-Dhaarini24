b86c8b72dc9b319193300de8acfe584c29213e53
Dhaarini Hathwar <96413511+Dhaarini24@users.noreply.github.com>
2022-05-10 15:05:55 +0000

    4

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,12 +1,14 @@
-# Functions
-
-
-def computepay(h, r):
-    pass  # ...
-
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
-print("Pay", p)
+score = input("Enter Score: ")
+i=float(score) 
+if i>=0.9 and i<=1.0:
+     print("A")
+elif i>=0.8 and i<0.9:
+     print("B")
+elif i>=0.7 and i<0.8:
+     print("C")
+elif i>=0.6 and i<0.7:
+     print("D")
+elif i<0.6 and i>=0.0:
+     print("F")
+else:
+    print("error, print the number in the suitable range")
