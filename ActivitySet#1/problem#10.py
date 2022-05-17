d82116d05fb41e6bc23697f39afa29f669d5a17c
Dhaarini Hathwar <96413511+Dhaarini24@users.noreply.github.com>
2022-05-17 15:41:54 +0000

    .

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -7,14 +7,14 @@ for line in handle:
     if not line.startswith("From:"):
         continue
     words=line.split()
-    x=(words[1:])
-    for word in x:
+    y=(words[1:])
+    for word in y:
         counts[word]=counts.get(word,0)+1
  
     largestcount = 0
     largestword =0
     for word,count in counts.items():
-        if largestcount is 0 or count>largestcount:
+        if largestcount is 0 or count > largestcount:
            largestword=word
            largestcount=count
 print(largestword,largestcount)        
