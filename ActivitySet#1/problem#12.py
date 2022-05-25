e9790a3009e959dbbb43b9eb6dd7ee0be8e4ff6d
Dhaarini Hathwar <96413511+Dhaarini24@users.noreply.github.com>
2022-05-25 03:52:48 +0000

    .

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,13 +1,13 @@
-fname = input("Enter file name: ")
-if len(fname) < 1:
-    fname = "mbox-short.txt"
-
-fh = open(fname)
-count = 0
-for line in fh:
-    if not line.startswith('From:'):
-        continue
-    words= line.split()
-
-
+import re
+file=input('enter the  file name')
+fhand=open(file)
+number=list()
+for line in fhand:
+  x=line.rstrip()
+  y=re.findall('[0-9]+',x)
+  for a in y:
+    a=int(y)
+    number.append(a)
+print(sum(number))
     
+  
